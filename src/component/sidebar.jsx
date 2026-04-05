b47d12598d6ac0f33e6757a46393ef8bf757271a
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-04-06 01:00:34 +0600

    Simplify routing structure

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -1,74 +1,158 @@
 import { NavLink } from "react-router-dom";
-import { sidebarSections } from "../routes/routeConfig.jsx";
-
-function SidebarLink({ item }) {
-  return (
-    <NavLink className="nav-link" to={item.path}>
-      {item.label}
-    </NavLink>
-  );
-}
-
-function SidebarCollapse({ item, parentId }) {
-  const childHasNestedCollapses = item.children?.some((child) => child.type === "collapse");
-  const childNavClass = childHasNestedCollapses
-    ? "sidenav-menu-nested nav accordion"
-    : "sidenav-menu-nested nav";
-
-  return (
-    <>
-      <a
-        className="nav-link collapsed"
-        href="#!"
-        data-bs-toggle="collapse"
-        data-bs-target={`#${item.id}`}
-        aria-expanded="false"
-        aria-controls={item.id}
-      >
-        {item.icon ? (
-          <div className="nav-link-icon">
-            <i data-feather={item.icon} />
-          </div>
-        ) : null}
-        {item.label}
-        <div className="sidenav-collapse-arrow">
-          <i className="fas fa-angle-down" />
-        </div>
-      </a>
-
-      <div className="collapse" id={item.id} data-bs-parent={parentId ? `#${parentId}` : undefined}>
-        <nav className={childNavClass} id={item.menuId}>
-          <SidebarItems items={item.children} parentId={item.menuId} />
-        </nav>
-      </div>
-    </>
-  );
-}
-
-function SidebarItems({ items, parentId }) {
-  return items.map((item) => {
-    if (item.type === "heading") {
-      return (
-        <div className="sidenav-menu-heading" key={item.label}>
-          {item.label}
-        </div>
-      );
-    }
-
-    if (item.type === "collapse") {
-      return <SidebarCollapse item={item} parentId={parentId} key={item.id} />;
-    }
-
-    return <SidebarLink item={item} key={item.path} />;
-  });
-}
 
 function Sidebar() {
   return (
     <nav className="sidenav shadow-right sidenav-light">
       <div className="sidenav-menu">
         <div className="nav accordion" id="accordionSidenav">
-          <SidebarItems items={sidebarSections} parentId="accordionSidenav" />
+          <div className="sidenav-menu-heading">Core</div>
+          <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#collapseDashboards">
+            <div className="nav-link-icon"><i data-feather="activity" /></div>
+            Dashboards
+            <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+          </a>
+          <div className="collapse" id="collapseDashboards" data-bs-parent="#accordionSidenav">
+            <nav className="sidenav-menu-nested nav">
+              <NavLink className="nav-link" to="/default-dashboard">Default</NavLink>
+              <NavLink className="nav-link" to="/dashboard">Affiliate</NavLink>
+            </nav>
+          </div>
+
+          <div className="sidenav-menu-heading">Custom</div>
+          <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#collapsePages">
+            <div className="nav-link-icon"><i data-feather="grid" /></div>
+            Pages
+            <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+          </a>
+          <div className="collapse" id="collapsePages" data-bs-parent="#accordionSidenav">
+            <nav className="sidenav-menu-nested nav accordion" id="accordionSidenavPagesMenu">
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAccount">
+                Account
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="pagesCollapseAccount" data-bs-parent="#accordionSidenavPagesMenu">
+                <nav className="sidenav-menu-nested nav">
+                  <NavLink className="nav-link" to="/account-profile">Profile</NavLink>
+                  <NavLink className="nav-link" to="/account-billing">Billing</NavLink>
+                  <NavLink className="nav-link" to="/account-security">Security</NavLink>
+                  <NavLink className="nav-link" to="/account-notifications">Notifications</NavLink>
+                </nav>
+              </div>
+
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth">
+                Authentication
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="pagesCollapseAuth" data-bs-parent="#accordionSidenavPagesMenu">
+                <nav className="sidenav-menu-nested nav accordion" id="accordionSidenavPagesAuth">
+                  <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuthBasic">
+                    Basic
+                    <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+                  </a>
+                  <div className="collapse" id="pagesCollapseAuthBasic" data-bs-parent="#accordionSidenavPagesAuth">
+                    <nav className="sidenav-menu-nested nav">
+                      <NavLink className="nav-link" to="/auth-login-basic">Login</NavLink>
+                      <NavLink className="nav-link" to="/auth-register-basic">Register</NavLink>
+                      <NavLink className="nav-link" to="/auth-password-basic">Forgot Password</NavLink>
+                    </nav>
+                  </div>
+
+                  <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuthSocial">
+                    Social
+                    <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+                  </a>
+                  <div className="collapse" id="pagesCollapseAuthSocial" data-bs-parent="#accordionSidenavPagesAuth">
+                    <nav className="sidenav-menu-nested nav">
+                      <NavLink className="nav-link" to="/auth-login-social">Login</NavLink>
+                      <NavLink className="nav-link" to="/auth-register-social">Register</NavLink>
+                      <NavLink className="nav-link" to="/auth-password-social">Forgot Password</NavLink>
+                    </nav>
+                  </div>
+                </nav>
+              </div>
+
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError">
+                Error
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="pagesCollapseError" data-bs-parent="#accordionSidenavPagesMenu">
+                <nav className="sidenav-menu-nested nav">
+                  <NavLink className="nav-link" to="/error-400">400 Error</NavLink>
+                  <NavLink className="nav-link" to="/error-401">401 Error</NavLink>
+                  <NavLink className="nav-link" to="/error-403">403 Error</NavLink>
+                  <NavLink className="nav-link" to="/error-404-1">404 Error 1</NavLink>
+                  <NavLink className="nav-link" to="/error-404-2">404 Error 2</NavLink>
+                  <NavLink className="nav-link" to="/error-500">500 Error</NavLink>
+                  <NavLink className="nav-link" to="/error-503">503 Error</NavLink>
+                  <NavLink className="nav-link" to="/error-504">504 Error</NavLink>
+                </nav>
+              </div>
+
+              <NavLink className="nav-link" to="/pricing">Pricing</NavLink>
+              <NavLink className="nav-link" to="/invoice">Invoice</NavLink>
+            </nav>
+          </div>
+
+          <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#collapseApps">
+            <div className="nav-link-icon"><i data-feather="globe" /></div>
+            Applications
+            <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+          </a>
+          <div className="collapse" id="collapseApps" data-bs-parent="#accordionSidenav">
+            <nav className="sidenav-menu-nested nav accordion" id="accordionSidenavAppsMenu">
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#appsCollapseKnowledgeBase">
+                Knowledge Base
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="appsCollapseKnowledgeBase" data-bs-parent="#accordionSidenavAppsMenu">
+                <nav className="sidenav-menu-nested nav">
+                  <NavLink className="nav-link" to="/knowledge-base-home-1">Home 1</NavLink>
+                  <NavLink className="nav-link" to="/knowledge-base-home-2">Home 2</NavLink>
+                  <NavLink className="nav-link" to="/knowledge-base-category">Category</NavLink>
+                  <NavLink className="nav-link" to="/knowledge-base-article">Article</NavLink>
+                </nav>
+              </div>
+
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#appsCollapseUserManagement">
+                User Management
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="appsCollapseUserManagement" data-bs-parent="#accordionSidenavAppsMenu">
+                <nav className="sidenav-menu-nested nav">
+                  <NavLink className="nav-link" to="/user-management-list">Users List</NavLink>
+                  <NavLink className="nav-link" to="/user-management-edit-user">Edit User</NavLink>
+                  <NavLink className="nav-link" to="/user-management-add-user">Add User</NavLink>
+                  <NavLink className="nav-link" to="/user-management-groups-list">Groups List</NavLink>
+                  <NavLink className="nav-link" to="/user-management-org-details">Organization Details</NavLink>
+                </nav>
+              </div>
+
+              <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#appsCollapsePostsManagement">
+                Posts Management
+                <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+              </a>
+              <div className="collapse" id="appsCollapsePostsManagement" data-bs-parent="#accordionSidenavAppsMenu">
+                <nav className="sidenav-menu-nested nav">
+                  <NavLink className="nav-link" to="/blog-management-posts-list">Posts List</NavLink>
+                  <NavLink className="nav-link" to="/blog-management-create-post">Create Post</NavLink>
+                  <NavLink className="nav-link" to="/blog-management-edit-post">Edit Post</NavLink>
+                  <NavLink className="nav-link" to="/blog-management-posts-admin">Posts Admin</NavLink>
+                </nav>
+              </div>
+            </nav>
+          </div>
+
+          <a className="nav-link collapsed" href="#!" data-bs-toggle="collapse" data-bs-target="#collapseFlows">
+            <div className="nav-link-icon"><i data-feather="repeat" /></div>
+            Flows
+            <div className="sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
+          </a>
+          <div className="collapse" id="collapseFlows" data-bs-parent="#accordionSidenav">
+            <nav className="sidenav-menu-nested nav">
+              <NavLink className="nav-link" to="/multi-tenant-select">Multi-Tenant Registration</NavLink>
+              <NavLink className="nav-link" to="/wizard">Wizard</NavLink>
+            </nav>
+          </div>
         </div>
       </div>
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,97 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { appRoutes } from "./routeConfig.jsx";
+import Invoice from "../pages/Invoice.jsx";
+import Pricing from "../pages/Pricing.jsx";
+import AccountBilling from "../pages/account/AccountBilling.jsx";
+import AccountNotifications from "../pages/account/AccountNotifications.jsx";
+import AccountProfile from "../pages/account/AccountProfile.jsx";
+import AccountSecurity from "../pages/account/AccountSecurity.jsx";
+import BlogManagementCreatePost from "../pages/applications/BlogManagementCreatePost.jsx";
+import BlogManagementEditPost from "../pages/applications/BlogManagementEditPost.jsx";
+import BlogManagementPostsAdmin from "../pages/applications/BlogManagementPostsAdmin.jsx";
+import BlogManagementPostsList from "../pages/applications/BlogManagementPostsList.jsx";
+import KnowledgeBaseArticle from "../pages/applications/KnowledgeBaseArticle.jsx";
+import KnowledgeBaseCategory from "../pages/applications/KnowledgeBaseCategory.jsx";
+import KnowledgeBaseHomeOne from "../pages/applications/KnowledgeBaseHomeOne.jsx";
+import KnowledgeBaseHomeTwo from "../pages/applications/KnowledgeBaseHomeTwo.jsx";
+import UserManagementAddUser from "../pages/applications/UserManagementAddUser.jsx";
+import UserManagementEditUser from "../pages/applications/UserManagementEditUser.jsx";
+import UserManagementGroupsList from "../pages/applications/UserManagementGroupsList.jsx";
+import UserManagementList from "../pages/applications/UserManagementList.jsx";
+import UserManagementOrgDetails from "../pages/applications/UserManagementOrgDetails.jsx";
+import AuthLoginBasic from "../pages/auth/AuthLoginBasic.jsx";
+import AuthLoginSocial from "../pages/auth/AuthLoginSocial.jsx";
+import AuthPasswordBasic from "../pages/auth/AuthPasswordBasic.jsx";
+import AuthPasswordSocial from "../pages/auth/AuthPasswordSocial.jsx";
+import AuthRedirect from "../pages/auth/AuthRedirect.jsx";
+import AuthRegisterBasic from "../pages/auth/AuthRegisterBasic.jsx";
+import AuthRegisterSocial from "../pages/auth/AuthRegisterSocial.jsx";
+import Dashboard from "../pages/dashboards/Dashboard.jsx";
+import DefaultDashboard from "../pages/dashboards/DefaultDashboard.jsx";
+import Error400 from "../pages/errors/Error400.jsx";
+import Error401 from "../pages/errors/Error401.jsx";
+import Error403 from "../pages/errors/Error403.jsx";
+import Error404One from "../pages/errors/Error404One.jsx";
+import Error404Two from "../pages/errors/Error404Two.jsx";
+import Error500 from "../pages/errors/Error500.jsx";
+import Error503 from "../pages/errors/Error503.jsx";
+import Error504 from "../pages/errors/Error504.jsx";
+import MultiTenantAddUsers from "../pages/flows/MultiTenantAddUsers.jsx";
+import MultiTenantCreate from "../pages/flows/MultiTenantCreate.jsx";
+import MultiTenantJoin from "../pages/flows/MultiTenantJoin.jsx";
+import MultiTenantSelect from "../pages/flows/MultiTenantSelect.jsx";
+import Wizard from "../pages/flows/Wizard.jsx";
 
 function AppRoutes() {
   return (
     <Routes>
-      {appRoutes.map(({ path, element: Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/default-dashboard" element={<DefaultDashboard />} />
+
+      <Route path="/account-profile" element={<AccountProfile />} />
+      <Route path="/account-billing" element={<AccountBilling />} />
+      <Route path="/account-security" element={<AccountSecurity />} />
+      <Route path="/account-notifications" element={<AccountNotifications />} />
+
+      <Route path="/auth-login-basic" element={<AuthLoginBasic />} />
+      <Route path="/auth-register-basic" element={<AuthRegisterBasic />} />
+      <Route path="/auth-password-basic" element={<AuthPasswordBasic />} />
+      <Route path="/auth-login-social" element={<AuthLoginSocial />} />
+      <Route path="/auth-register-social" element={<AuthRegisterSocial />} />
+      <Route path="/auth-password-social" element={<AuthPasswordSocial />} />
+      <Route path="/auth-redirect" element={<AuthRedirect />} />
+
+      <Route path="/error-400" element={<Error400 />} />
+      <Route path="/error-401" element={<Error401 />} />
+      <Route path="/error-403" element={<Error403 />} />
+      <Route path="/error-404-1" element={<Error404One />} />
+      <Route path="/error-404-2" element={<Error404Two />} />
+      <Route path="/error-500" element={<Error500 />} />
+      <Route path="/error-503" element={<Error503 />} />
+      <Route path="/error-504" element={<Error504 />} />
+
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/invoice" element={<Invoice />} />
+
+      <Route path="/knowledge-base-home-1" element={<KnowledgeBaseHomeOne />} />
+      <Route path="/knowledge-base-home-2" element={<KnowledgeBaseHomeTwo />} />
+      <Route path="/knowledge-base-category" element={<KnowledgeBaseCategory />} />
+      <Route path="/knowledge-base-article" element={<KnowledgeBaseArticle />} />
+      <Route path="/user-management-list" element={<UserManagementList />} />
+      <Route path="/user-management-edit-user" element={<UserManagementEditUser />} />
+      <Route path="/user-management-add-user" element={<UserManagementAddUser />} />
+      <Route path="/user-management-groups-list" element={<UserManagementGroupsList />} />
+      <Route path="/user-management-org-details" element={<UserManagementOrgDetails />} />
+      <Route path="/blog-management-posts-list" element={<BlogManagementPostsList />} />
+      <Route path="/blog-management-create-post" element={<BlogManagementCreatePost />} />
+      <Route path="/blog-management-edit-post" element={<BlogManagementEditPost />} />
+      <Route path="/blog-management-posts-admin" element={<BlogManagementPostsAdmin />} />
+
+      <Route path="/multi-tenant-select" element={<MultiTenantSelect />} />
+      <Route path="/multi-tenant-join" element={<MultiTenantJoin />} />
+      <Route path="/multi-tenant-create" element={<MultiTenantCreate />} />
+      <Route path="/multi-tenant-add-users" element={<MultiTenantAddUsers />} />
+      <Route path="/wizard" element={<Wizard />} />
+
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
